1a53a118a1d32f2c0c47ea7cdfa8612487c49c18
HITlgw <2035741499@qq.com>
2022-04-20 08:12:49 +0800

    V4 策略模式和Dao模式

--- src/edu/hitsz/aircraftFactory/BossEnemyFactory.java
+++ src/edu/hitsz/aircraftFactory/BossEnemyFactory.java
@@ -10,7 +10,7 @@ public class BossEnemyFactory implements EnemyAircraftFactory {
     @Override
     public AbstractAircraft createEnemyAircraft() {
         AbstractAircraft boss = new BossEnemy(Main.WINDOW_WIDTH / 2,
-                ImageManager.BOSS_ENEMY_IMAGE.getHeight(),
+                ImageManager.BOSS_ENEMY_IMAGE.getHeight()/2,
                 5, 0, 300);
         boss.setShootingType(new DiffuseShootingType());
         return boss;
